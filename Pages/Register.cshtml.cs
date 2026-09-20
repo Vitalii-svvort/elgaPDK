cd3291e5e53d9271d52082d81fa823ba94114686
Vitalii-svvort <svvort@yandex.ru>
2026-09-20 16:41:04 +0300

    Update Register.cshtml.cs

--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -1,1 +1,42 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace MeetingProtocols.Pages
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public string Email { get; set; } = "";
+        [BindProperty]
+        public string Password { get; set; } = "";
+
+        public string Message { get; set; } = "";
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = new IdentityUser { UserName = Email, Email = Email };
+            var result = await _userManager.CreateAsync(user, Password);
+
+            if (result.Succeeded)
+            {
+                // Пользователь создан, можно сразу логинить или перенаправить на вход
+                Message = "Регистрация успешна! Теперь войдите.";
+                return Page();
+            }
+            else
+            {
+                Message = "Ошибка регистрации: " + string.Join(", ", result.Errors.Select(e => e.Description));
+                return Page();
+            }
+        }
+    }
+}
